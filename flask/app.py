627eb08f648558ca04f3ae98cd34508ba92c4ea5
starterpoint <hulk301080@gmail.com>
2026-07-08 15:43:20 +0330

    add json files and todos

--- flask/app.py
+++ flask/app.py
@@ -1,15 +1,28 @@
+from flask import Flask, jsonify
 from datetime import datetime
-from flask import Flask
 
 app = Flask(__name__)
 
+todos = [
+    {"id": 1, "title": "Learn Docker"},
+    {"id": 2, "title": "Learn Flask"},
+]
+
+
 @app.route("/")
 def home():
-    return "Hello from Docker and it's working on your VPS!"
+    return "Hello from VPS!"
+
 
 @app.route("/time")
 def time():
     return str(datetime.now())
+
+
+@app.route("/todos")
+def get_todos():
+    return jsonify(todos)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
