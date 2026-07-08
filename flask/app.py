49110c0b98569797979f7b81a1d044ff215e3537
starterpoint <hulk301080@gmail.com>
2026-07-08 14:19:20 +0330

    Initial project setup

---
+++ flask/app.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Docker!"
+
+@app.route("/time")
+def time():
+    return str(datetime.now())
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
