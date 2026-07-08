331e6028e21af00352bf9e448261658dc96b50cf
starterpoint <hulk301080@gmail.com>
2026-07-08 19:48:37 +0330

    add POST methode to app.py flask code

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from datetime import datetime
 
 app = Flask(__name__)
@@ -19,10 +19,18 @@ def time():
     return str(datetime.now())
 
 
-@app.route("/todos")
-def get_todos():
-    return jsonify(todos)
+@app.route("/todos", methods=["POST"])
+def create_todo():
+    data = request.get_json()
 
+    todo = {
+        "id": len(todos) + 1,
+        "title": data["title"]
+    }
+
+    todos.append(todo)
+
+    return jsonify(todo), 201
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
