3c5b2315c88f7eedbf7d9acb4281ec5dab839366
starterpoint <hulk301080@gmail.com>
2026-07-14 16:10:25 +0330

    modifing monitor.sh function and add print()

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -1,7 +1,12 @@
+STATUS=0
+
+DISK_USAGE=0
+RAM_USAGE=0
+
+
 #!/bin/bash
 
 STATUS=0
-
 
 check_disk() {
 
@@ -9,28 +14,44 @@ check_disk() {
 
     DISK_USAGE=$(df / | awk 'NR==2 {print $5}' | sed 's/%//')
 
-    echo "Disk Usage: ${DISK_USAGE}%"
-
     if [ "$DISK_USAGE" -ge "$THRESHOLD" ]; then
-
-        echo "WARNING: Disk usage is high."
-
         STATUS=1
+    fi
 
+}
+
+check_ram() {
+
+    RAM_THRESHOLD=90
+
+    RAM_USAGE=$(free | awk '/Mem:/ {printf("%.0f"), $3/$2 * 100}')
+
+    if [ "$RAM_USAGE" -ge "$RAM_THRESHOLD" ]; then
+        STATUS=1
     fi
 
 }
 
 
-main() {
+print_report() {
 
-    check_disk
+    echo "DISK_USAGE=$DISK_USAGE"
+    echo "RAM_USAGE=$RAM_USAGE"
 
     if [ "$STATUS" -eq 0 ]; then
         echo "STATUS=OK"
     else
         echo "STATUS=WARNING"
     fi
+
+}
+
+main() {
+
+    check_disk
+    check_ram
+
+    print_report
 
     exit "$STATUS"
 
