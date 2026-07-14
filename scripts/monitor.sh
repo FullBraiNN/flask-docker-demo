571758c8c8363f7f14d3ded973fc270cc85b13f6
starterpoint <hulk301080@gmail.com>
2026-07-14 14:13:35 +0330

    create monitor.yml and modified monitor.sh

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -7,7 +7,9 @@ DISK_USAGE=$(df / | awk 'NR==2 {print $5}' | sed 's/%//')
 echo "Disk Usage: ${DISK_USAGE}%"
 
 if [ "$DISK_USAGE" -ge "$THRESHOLD" ]; then
-    echo "WARNING: Disk usage is above ${THRESHOLD}%"
+    echo "STATUS=WARNING"
+    exit 1
 else
-    echo "Disk usage is normal."
+    echo "STATUS=OK"
+    exit 0
 fi
